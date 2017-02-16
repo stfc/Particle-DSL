371b472a16ed763476e2b08e80cd53e8a967424c
Andrew Porter <andrew.porter@stfc.ac.uk>
2017-02-16 10:29:33 +0000

    Simplify and comment top-level driver code

--- muMD/psykal-vanilla/source/muMD.F90
+++ muMD/psykal-vanilla/source/muMD.F90
@@ -1,7 +1,7 @@
-!    Copyright (c) 2016-2016 Alin Marin Elena <alinm.elena@gmail.com>
-!    The MIT License http://opensource.org/licenses/MIT
+!  Copyright (c) 2016-2016 Alin Marin Elena <alinm.elena@gmail.com>
+!  Modifications Copyright (c) 2017 Science and Technology Facilities Council
+!  The MIT License http://opensource.org/licenses/MIT
 program microMD
-  ! the name shall be μMD not microMD
   use m_Constants
   use m_Types
   use m_Useful, only: DateAndTime, error, getVdw,hs
@@ -18,72 +18,83 @@ program microMD
   integer :: ierror
   character (len=k_mw) :: arg
   character (len=k_ml) :: dummy
-  character (len=10) :: dt
-  character (len=12) :: tm
+  character (len=10) :: dt_str
+  character (len=12) :: tm_str
 
   type(ioType)        :: io
   type(controlType)   :: control
   type(fieldType)     :: field
   type(particlesType) :: particles
+  integer :: i,sp,t
+  real(rp) :: dt=0.001_rp, im 
 
-  call DateAndTime (dt, tm)
-  write (dummy, '(a,a,a,a)') "Program µMD has started at ", dt, " ", tm
+  call DateAndTime (dt_str, tm_str)
+  write (dummy, '(a,a,a,a)') "Program µMD has started at ", dt_str, " ", tm_str
   io%controlFile = "CONTROL"
   if (command_argument_count()==1) then
     call get_command_argument(1,io%controlFile)
   end if
   call readControl(io,control)
   call compilerInfo(io)
-  write (io%uout, '(a)')trim(dummy) 
+  write (io%uout, '(a)') trim(dummy) 
   call readConfig(particles,io)
   call particles%summary(io)
   call readField(io,field,particles)
   call particles%fieldSummary(io)
   call setupNeighbours(particles)
 
-  block
-    integer :: i,sp,t
-    real(rp) :: dt=0.001_rp,im 
-    call buildNeighbours(particles,control%rc)
+  call buildNeighbours(particles,control%rc)
 
-    call computeForces(particles,control)
-    write(*,*)"total energy", particles%energy()
-    if (io%isTraj) then
-      call particles%writeTrajectory(io,0,0.0_rp,0.0_rp,isFirst=.true.,level=2)
-    endif
+  call computeForces(particles,control)
+  write(*,*)"total energy", particles%energy()
+  if (io%isTraj) then
+     call particles%writeTrajectory(io,0,0.0_rp,0.0_rp,isFirst=.true.,level=2)
+  endif
 
-    do t=1,control%steps
-      control%time=control%time+control%timestep
-      control%step=control%step+1
-      do i=1,particles%nGParticles
+  ! Begin time-stepping loop
+  do t=1,control%steps
+
+     control%time=control%time+control%timestep
+     control%step=control%step+1
+
+     ! Update velocities, first stage
+     do i=1,particles%nGParticles
         sp=particles%spec(i)
         im=control%timestep/(2.0_rp*particles%mass(sp))
         particles%vx(i) = particles%vx(i) + im*particles%fx(i)
         particles%vy(i) = particles%vy(i) + im*particles%fy(i)
         particles%vz(i) = particles%vz(i) + im*particles%fz(i)
-      enddo
-      do i=1,particles%nGParticles
+     enddo
+
+     ! Update positions
+     do i=1,particles%nGParticles
         particles%x(i) = particles%x(i) + particles%vx(i)*dt 
         particles%y(i) = particles%y(i) + particles%vy(i)*dt 
         particles%z(i) = particles%z(i) + particles%vz(i)*dt 
-      enddo
-      call computeForces(particles,control)
-      do i=1,particles%nGParticles
+     enddo
+
+     call computeForces(particles,control)
+
+     ! Update velocities using computed forces, second stage
+     do i=1,particles%nGParticles
         sp=particles%spec(i)
         im=control%timestep/(2.0_rp*particles%mass(sp))
         particles%vx(i) = particles%vx(i) + im*particles%fx(i)
         particles%vy(i) = particles%vy(i) + im*particles%fy(i)
         particles%vz(i) = particles%vz(i) + im*particles%fz(i)
-      enddo
-      write(*,*)"Centre of Mass", particles%centreOfMass()
-      write(*,*)"total energy", particles%energy()/engUnits, trim(field%units)
-      if (io%isTraj) then
-        call particles%writeTrajectory(io,control%step,control%time,control%timestep,isFirst=.false.,level=2)
-      endif 
-    enddo
-  end block
+     enddo
+
+     write(*,*)"Centre of Mass", particles%centreOfMass()
+     write(*,*)"total energy", particles%energy()/engUnits, trim(field%units)
+     if (io%isTraj) then
+        call particles%writeTrajectory(io,control%step,control%time, &
+                                       control%timestep,isFirst=.false., &
+                                       level=2)
+     endif
+  enddo
+
   call closeIO(io)
-  call DateAndTime (dt, tm)
-  write (io%uout, '(a,a,a,a)') "Program µMD has ended at ", dt, " ", tm
+  call DateAndTime (dt_str, tm_str)
+  write (io%uout, '(a,a,a,a)') "Program µMD has ended at ", dt_str, " ", tm_str
 
 end program microMD
